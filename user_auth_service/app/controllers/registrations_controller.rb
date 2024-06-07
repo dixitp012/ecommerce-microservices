5a41018fcd11443accdc9666c73bd8b3a337c3d3
Dixit Patel <dixitpatel012@aol.com>
2024-06-08 04:04:04 +0530

    Improved spec after changes permited params

--- user_auth_service/app/controllers/registrations_controller.rb
+++ user_auth_service/app/controllers/registrations_controller.rb
@@ -12,6 +12,6 @@ class RegistrationsController < ApplicationController
 
     private
       def user_params
-        params.permit(:email, :password, :password_confirmation)
+        params.require(:user).permit(:email, :password, :password_confirmation)
       end
 end
--- user_auth_service/spec/controllers/registrations_controller_spec.rb
+++ user_auth_service/spec/controllers/registrations_controller_spec.rb
@@ -4,17 +4,21 @@ RSpec.describe RegistrationsController, type: :controller do
   describe 'POST #create' do
     context 'with valid params' do
       it 'creates a new User' do
-        post :create, params: attributes_for(:user)
+        expect {
+          post :create, params: { user: attributes_for(:user) }
+        }.to change(User, :count).by(1)
+        
         expect(response).to have_http_status(:created)
-        expect(User.count).to eq(1)
+        expect(JSON.parse(response.body)['email']).to eq(User.last.email)
       end
     end
 
     context 'with invalid params' do
       it 'returns an unprocessable entity status' do
-        post :create, params: attributes_for(:user, password_confirmation: 'mismatch')
+        post :create, params: { user: attributes_for(:user, password_confirmation: 'mismatch') }
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
 end
+
